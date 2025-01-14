d0849f3ab8e3276a8e6e0ee9eb18471a2ec0ecab
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-01-14 09:22:51 +0100

    MONDRE JAJA JAKIES PTASIE

--- examples/default.rs
+++ examples/default.rs
@@ -1,12 +1,20 @@
-use bevy::{app::{App, Startup}, DefaultPlugins};
+use bevy::{app::{App, PluginGroup, Startup}, window::{Window, WindowPlugin}, DefaultPlugins};
 
 fn main() {
 
     // Uruchamianie gry wraz z domyślnymi pluginami (np. obsługa okna, renderowanie, obsługa wejścia)
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins::set(
+            DefaultPlugins, WindowPlugin { // Modyfikowanie configuracji pluginu Window (np. tytuł okna)
+            primary_window: Some(Window {
+                title: "Hello world!".to_string(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
         .add_systems(Startup, helloworld) // Startup to schedule który uruchomi system/funkcję przy starcie programu
         .run();
+    
 
 }
 
