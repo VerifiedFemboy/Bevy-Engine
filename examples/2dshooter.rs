a8514d62b727e73db7ad45e59077070edccfc3fc
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-01-17 13:00:50 +0100

    events jaja xd

---
+++ examples/2dshooter.rs
@@ -0,0 +1,96 @@
+use bevy::{app::{App, PluginGroup, Update}, window::{Window, WindowPlugin}, DefaultPlugins};
+use camera::CameraPlugin;
+use player::PlayerPlugin;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins::set(DefaultPlugins, 
+        WindowPlugin {
+            primary_window: Some(Window {
+                title: "Shooter 2D".to_string(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_plugins((CameraPlugin, PlayerPlugin))
+        .run();
+}
+
+mod camera {
+    use bevy::prelude::*;
+
+    pub struct CameraPlugin;
+
+    impl Plugin for CameraPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_systems(Startup, setup);
+        }
+    }
+
+    fn setup(mut commands: Commands) {
+        commands.spawn(Camera2d);
+    }
+}
+
+mod player {
+    use bevy::prelude::*;
+
+    #[derive(Component)]
+    pub struct Player;
+
+    pub struct PlayerPlugin;
+
+    impl Plugin for PlayerPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_systems(Startup, setup);
+            app.add_event::<ShootEvent>();
+            // app.add_systems(Update, (movement_input, send_shoot_ev));
+            
+        }
+    }
+
+    fn setup(
+        mut commands: Commands, 
+        mut meshes: ResMut<Assets<Mesh>>, 
+        mut materials: ResMut<Assets<ColorMaterial>>
+    ) {
+        commands.spawn((Player, Transform::default(), 
+        Mesh2d(meshes.add(Circle::new(20.))),
+        MeshMaterial2d(materials.add(Color::WHITE))));
+    }
+
+    fn movement_input(
+        input: Res<ButtonInput<KeyCode>>, 
+        mut player_move: Query<&mut Transform, With<Player>>
+    ) {
+        for mut transform in player_move.iter_mut() {
+            if input.pressed(KeyCode::KeyW) {
+                transform.translation.y += 2.;
+            }
+            if input.pressed(KeyCode::KeyS) {
+                transform.translation.y -= 2.;
+            }
+            if input.pressed(KeyCode::KeyA) {
+                transform.translation.x -= 2.;
+            }
+
+            if input.pressed(KeyCode::KeyD) {
+                transform.translation.x += 2.;
+            }
+        }
+    }
+
+    #[derive(Event)]
+    struct ShootEvent(String);
+
+    fn send_shoot_ev(input: ButtonInput<MouseButton>, mut events: EventWriter<ShootEvent>) {
+        if input.just_pressed(MouseButton::Left) {
+            events.send(ShootEvent("jaja".to_string()));
+        }
+    }
+
+    fn read_shoot_ev(mut event: EventReader<ShootEvent>) {
+        for ev in event.read() {
+            println!("shot")
+        }
+    }
+}
