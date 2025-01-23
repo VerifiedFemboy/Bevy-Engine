f074b30eeef58328d9646172949a3149c6e08ff9
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-01-23 12:42:11 +0100

    dodane jondra

--- examples/solar.rs
+++ examples/solar.rs
@@ -1,22 +1,25 @@
-use bevy::{app::{App, PluginGroup, Startup}, asset::Assets, color::Color, core_pipeline::bloom::Bloom, prelude::{Camera, Camera2d, Circle, Commands, Component, IntoSystemConfigs, Mesh, Mesh2d, ResMut, Text, Transform}, sprite::{ColorMaterial, MeshMaterial2d}, text::Text2d, ui::Node, window::{Window, WindowPlugin}, DefaultPlugins};
+use bevy::{app::{App, PluginGroup, Startup, Update}, asset::Assets, color::Color, core_pipeline::bloom::Bloom, prelude::{Camera, Camera2d, Circle, Commands, Component, IntoSystemConfigs, Mesh, Mesh2d, Query, ResMut, Transform}, sprite::{ColorMaterial, MeshMaterial2d}, text::Text2d, window::{Window, WindowPlugin, WindowResolution}, DefaultPlugins};
 
 fn main() {
     App::new().add_plugins(DefaultPlugins::set(DefaultPlugins, 
         WindowPlugin {
             primary_window: Some(Window {
                 title: "Solar System".to_string(),
+                resizable: false,
+                resolution: WindowResolution::new(1920., 1080.),
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .add_systems(Startup, (spawn_camera, date_spawn_text, background, spawn_objects).chain())
+        .add_systems(Update, (update_date_text).chain())
         .run();
 }
 
 fn background(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn(
         (Transform::default(),
-        Mesh2d(meshes.add(Circle::new(1000.))), 
+        Mesh2d(meshes.add(Circle::new(5000.))), 
         MeshMaterial2d(materials.add(Color::srgb(0., 0., 0.))))
     );
 }
@@ -24,6 +27,7 @@ fn background(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut mate
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((Camera2d, Camera {
         hdr: true,
+        
         ..Default::default()
     }, Bloom::NATURAL));
 }
@@ -64,7 +68,7 @@ fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut m
             acceleration: (0., 0.),
         },
             Mesh2d(meshes.add(Circle::new(5.))),
-            MeshMaterial2d(materials.add(Color::srgb(1., 0., 0.))),
+            MeshMaterial2d(materials.add(Color::srgb(0.5, 0.5, 0.5))),
             Transform::from_xyz(108., 0., 1.)
         ),
         (Object {
@@ -76,9 +80,33 @@ fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut m
             acceleration: (0., 0.),
         },
             Mesh2d(meshes.add(Circle::new(5.))),
-            MeshMaterial2d(materials.add(Color::srgb(0., 0., 1.))),
+            MeshMaterial2d(materials.add(Color::hsl(211., 0.93, 0.7))),
             Transform::from_xyz(149.6, 0., 0.1)
-        )
+        ),
+        (Object {
+            name: "Mars".to_string(),
+            kind: ObjectKind::Planet,
+            mass: 6.42e23,
+            radius: 3.3895e6,
+            velocity: (0., 2.4077e4),
+            acceleration: (0., 0.),
+        },
+            Mesh2d(meshes.add(Circle::new(3.))),
+            MeshMaterial2d(materials.add(Color::srgb(1., 0.3, 0.))),
+            Transform::from_xyz(227.9, 0., 1.)
+        ),
+        (Object {
+            name: "Jupiter".to_string(),
+            kind: ObjectKind::Planet,
+            mass: 1.90e27,
+            radius: 6.9911e7,
+            velocity: (0., 1.307e4),
+            acceleration: (0., 0.),
+        },
+            Mesh2d(meshes.add(Circle::new(10.))),
+            MeshMaterial2d(materials.add(Color::srgb(0.0, 0.5, 0.))),
+            Transform::from_xyz(778.6, 0., 1.)
+        ),
         ];
 
     for planet in planets {
@@ -87,9 +115,16 @@ fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut m
 }
 
 fn date_spawn_text(mut commands: Commands) {
-    commands.spawn((Text2d("Jaja".to_string()),
+    commands.spawn((Text2d(format!("Date: {}\nTime: {}", get_current_date_and_time().0, get_current_date_and_time().1)),
         Transform::from_xyz(-400., 300., 0.),
     ));
+}
+
+fn update_date_text(mut query: Query<&mut Text2d>) {
+    for mut text in query.iter_mut() {
+        text.0 = format!("Date: {}\nTime: {}", get_current_date_and_time().0, get_current_date_and_time().1);
+    }
+
 }
 
 #[derive(Component)]
@@ -106,4 +141,10 @@ enum ObjectKind {
     Planet,
     Star,
     Moon,
+}
+
+fn get_current_date_and_time() -> (String, String) {
+    let date = chrono::Utc::now().format("%m-%d-%Y").to_string();
+    let time = chrono::Utc::now().format("%I:%M:%S %p").to_string();
+    (date, time)
 }
