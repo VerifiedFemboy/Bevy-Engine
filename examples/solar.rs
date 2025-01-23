643b77fab2297a2874bb108d6fcbb364ab1d2d0e
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-01-23 10:20:21 +0100

    added some balls

--- examples/solar.rs
+++ examples/solar.rs
@@ -1,4 +1,4 @@
-use bevy::{app::{App, PluginGroup, Startup}, asset::Assets, color::Color, prelude::{Camera2d, Circle, Commands, Component, IntoSystemConfigs, Mesh, Mesh2d, ResMut, Transform}, sprite::{ColorMaterial, MeshMaterial2d}, window::{Window, WindowPlugin}, DefaultPlugins};
+use bevy::{app::{App, PluginGroup, Startup}, asset::Assets, color::Color, core_pipeline::bloom::Bloom, prelude::{Camera, Camera2d, Circle, Commands, Component, IntoSystemConfigs, Mesh, Mesh2d, ResMut, Transform}, sprite::{ColorMaterial, MeshMaterial2d}, window::{Window, WindowPlugin}, DefaultPlugins};
 
 fn main() {
     App::new().add_plugins(DefaultPlugins::set(DefaultPlugins, 
@@ -22,7 +22,10 @@ fn background(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut mate
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    commands.spawn((Camera2d, Camera {
+        hdr: true,
+        ..Default::default()
+    }, Bloom::NATURAL));
 }
 
 fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
@@ -36,10 +39,10 @@ fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut m
     },
         Mesh2d(meshes.add(Circle::new(20.))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 0.))),
-        Transform::default()
+        Transform::from_xyz(0., 0., 1.0)
     ));
 
-    let planets = vec![(
+    let planets = vec![
         (Object {
             name: "Mercury".to_string(),
             kind: ObjectKind::Planet,
@@ -48,10 +51,35 @@ fn spawn_objects(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut m
             velocity: (0., 4.787e4),
             acceleration: (0., 0.),
         },
+            Mesh2d(meshes.add(Circle::new(1.))),
+            MeshMaterial2d(materials.add(Color::srgb(1., 0.5, 0.))),
+            Transform::from_xyz(57., 0., 1.)
+        ),
+        (Object {
+            name: "Venus".to_string(),
+            kind: ObjectKind::Planet,
+            mass: 4.87e24,
+            radius: 6.0518e6,
+            velocity: (0., 3.502e4),
+            acceleration: (0., 0.),
+        },
+            Mesh2d(meshes.add(Circle::new(5.))),
+            MeshMaterial2d(materials.add(Color::srgb(1., 0., 0.))),
+            Transform::from_xyz(108., 0., 1.)
+        ),
+        (Object {
+            name: "Earth".to_string(),
+            kind: ObjectKind::Planet,
+            mass: 5.97e24,
+            radius: 6.371e6,
+            velocity: (0., 2.978e4),
+            acceleration: (0., 0.),
+        },
             Mesh2d(meshes.add(Circle::new(5.))),
             MeshMaterial2d(materials.add(Color::srgb(0., 0., 1.))),
-            Transform::from_xyz(57.9e9, 0., 0.)),
-    )];
+            Transform::from_xyz(149.6, 0., 0.1)
+        )
+        ];
 
     for planet in planets {
         commands.spawn(planet);
