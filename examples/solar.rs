b759b971614e1145f0e18c9cfd7e9fae76a1894b
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-01-25 13:49:24 +0100

    I hate ni-

--- examples/solar.rs
+++ examples/solar.rs
@@ -1,4 +1,13 @@
-use bevy::{app::{App, PluginGroup, Startup, Update}, asset::Assets, color::Color, core_pipeline::bloom::Bloom, input::ButtonInput, prelude::{BuildChildren, Camera, Camera2d, ChildBuild, Circle, Commands, Component, IntoSystemConfigs, KeyCode, Mesh, Mesh2d, OrthographicProjection, Query, Res, ResMut, Transform, With}, sprite::{ColorMaterial, MeshMaterial2d}, text::{Text2d, TextFont}, window::{Window, WindowPlugin, WindowResolution}, DefaultPlugins};
+use bevy::{app::{App, PluginGroup, Startup, Update}, 
+asset::Assets, color::Color, core_pipeline::bloom::Bloom, 
+input::{mouse::MouseWheel, ButtonInput}, math::Vec3, 
+prelude::{Annulus, BuildChildren, Camera, Camera2d, ChildBuild, Circle, 
+    Commands, Component, EventReader, IntoSystemConfigs, KeyCode, Mesh, 
+    Mesh2d, OrthographicProjection, Query, Res, ResMut, Transform, With}, 
+    sprite::{ColorMaterial, MeshMaterial2d}, text::{Text2d, TextFont}, 
+    time::Time, window::{Window, WindowPlugin, WindowResolution}, DefaultPlugins};
+
+//TODO: Sync our real time to the game time
 
 fn main() {
     App::new().add_plugins(DefaultPlugins::set(DefaultPlugins, 
@@ -11,8 +20,8 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .add_systems(Startup, (spawn_camera, date_spawn_text, background, spawn_objects).chain())
-        .add_systems(Update, (update_date_text, input_keys).chain())
+        .add_systems(Startup, (spawn_camera, date_spawn_text, background, spawn_objects, spawn_orbits).chain())
+        .add_systems(Update, (update_date_text, input_keys, update_zoom_by_scroll, update_planets_position).chain())
         .run();
 }
 
@@ -32,7 +41,7 @@ fn spawn_camera(mut commands: Commands) {
         hdr: true,
         ..Default::default()
     }, Bloom::NATURAL,
-        Transform::from_xyz(0., 0., 1000.), //TODO: SCALE THE CAMERA TO ZOOM OUT
+        Transform::from_xyz(0., 0., 0.),
         CameraEnt
     ));
 }
@@ -45,24 +54,22 @@ fn spawn_objects(
     commands.spawn((Object {
         name: "Sun".to_string(),
         kind: ObjectKind::Star,
-        mass: 1.989e30,
-        radius: 6.9634e8,
-        velocity: (0., 0.),
-        acceleration: (0., 0.),
+        radius: 6.9634,
+        speed: 0.,
+        angle: 0.,
     },
         Mesh2d(meshes.add(Circle::new(30.))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 0.))),
-        Transform::from_xyz(0., 0., 1.0)
+        Transform::from_xyz(0., 0., 1.)
     ));
 
     let planets = vec![
         (Object {
             name: "Mercury".to_string(),
             kind: ObjectKind::Planet,
-            mass: 3.30e23,
-            radius: 2.4397e6,
-            velocity: (0., 4.787e4),
-            acceleration: (0., 0.),
+            radius: 57.,
+            speed: 4.74,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(1.))),
             MeshMaterial2d(materials.add(Color::srgb(1., 0.5, 0.))),
@@ -71,10 +78,9 @@ fn spawn_objects(
         (Object {
             name: "Venus".to_string(),
             kind: ObjectKind::Planet,
-            mass: 4.87e24,
-            radius: 6.0518e6,
-            velocity: (0., 3.502e4),
-            acceleration: (0., 0.),
+            radius: 108.,
+            speed: 3.5,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(5.))),
             MeshMaterial2d(materials.add(Color::srgb(0.5, 0.5, 0.5))),
@@ -83,22 +89,20 @@ fn spawn_objects(
         (Object {
             name: "Earth".to_string(),
             kind: ObjectKind::Planet,
-            mass: 5.97e24,
-            radius: 6.371e6,
-            velocity: (0., 2.978e4),
-            acceleration: (0., 0.),
+            radius: 149.6,
+            speed: 2.98,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(5.))),
             MeshMaterial2d(materials.add(Color::hsl(211., 0.93, 0.7))),
-            Transform::from_xyz(149.6, 0., 0.1)
+            Transform::from_xyz(149.6, 0., 1.)
         ),
         (Object {
             name: "Mars".to_string(),
             kind: ObjectKind::Planet,
-            mass: 6.42e23,
-            radius: 3.3895e6,
-            velocity: (0., 2.4077e4),
-            acceleration: (0., 0.),
+            radius: 227.9,
+            speed: 2.41,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(3.))),
             MeshMaterial2d(materials.add(Color::srgb(1., 0.3, 0.))),
@@ -107,10 +111,9 @@ fn spawn_objects(
         (Object {
             name: "Jupiter".to_string(),
             kind: ObjectKind::Planet,
-            mass: 1.90e27,
-            radius: 6.9911e7,
-            velocity: (0., 1.307e4),
-            acceleration: (0., 0.),
+            radius: 778.6,
+            speed: 13.07,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(10.))),
             MeshMaterial2d(materials.add(Color::srgb(0.8, 0.7, 0.2))),
@@ -119,10 +122,9 @@ fn spawn_objects(
         (Object {
             name: "Saturn".to_string(),
             kind: ObjectKind::Planet,
-            mass: 5.68e26,
-            radius: 5.8232e7,
-            velocity: (0., 9.69e3),
-            acceleration: (0., 0.),
+            radius: 1433.5,
+            speed: 9.69,
+            angle: 0.,
         },
             Mesh2d(meshes.add(Circle::new(8.))),
             MeshMaterial2d(materials.add(Color::srgb(0.9, 0.8, 0.2))),
@@ -131,10 +133,9 @@ fn spawn_objects(
         (Object {
             name: "Uranus".to_string(),
             kind: ObjectKind::Planet,
-            mass: 8.68e25,
-            radius: 2.5362e7,
-            velocity: (0., 6.81e3),
-            acceleration: (0., 0.),
+            radius: 2872.5,
+            angle: 0.,
+            speed: 6.81,
         },
             Mesh2d(meshes.add(Circle::new(6.))),
             MeshMaterial2d(materials.add(Color::srgb(0.5, 0.8, 0.9))),
@@ -143,10 +144,9 @@ fn spawn_objects(
         (Object {
             name: "Neptune".to_string(),
             kind: ObjectKind::Planet,
-            mass: 1.02e26,
-            radius: 2.4622e7,
-            velocity: (0., 5.43e3),
-            acceleration: (0., 0.),
+            radius: 4495.1,
+            angle: 0.,
+            speed: 5.43,
         },
             Mesh2d(meshes.add(Circle::new(6.))),
             MeshMaterial2d(materials.add(Color::srgb(0.2, 0.3, 0.9))),
@@ -158,7 +158,7 @@ fn spawn_objects(
         let name = planet.0.name.clone();
         commands.spawn(planet
         ).with_children(|parent| {
-            parent.spawn((Text2d(name), TextFont {font_size: 6., ..Default::default()}, Transform::from_xyz(0., 5., 0.1)));
+            parent.spawn((Text2d(name), TextFont {font_size: 6., ..Default::default()}, Transform::from_xyz(0., 5., 0.2)));
         });
     }
 }
@@ -179,23 +179,20 @@ fn update_date_text(mut query: Query<&mut Text2d, With<DateText>>) {
     for mut text in query.iter_mut() {
         text.0 = format!("Date: {}\nTime: {}", get_current_date_and_time().0, get_current_date_and_time().1);
     }
-
 }
 
 #[derive(Component)]
 struct Object {
     name: String,
     kind: ObjectKind,
-    mass: f32,
     radius: f32,
-    velocity: (f32, f32),
-    acceleration: (f32, f32),
+    angle: f32,
+    speed: f32,
 }
 
 enum ObjectKind {
     Planet,
     Star,
-    Moon,
 }
 
 fn get_current_date_and_time() -> (String, String) {
@@ -220,18 +217,66 @@ fn input_keys(
     let mut position = position_query.single_mut();
 
     if key.pressed(KeyCode::KeyW) {
-        position.translation.y += 10.0;
+        position.translation.y += 5.0;
     }
     
     if key.pressed(KeyCode::KeyS) {
-        position.translation.y -= 10.0;
+        position.translation.y -= 5.0;
     }
 
     if key.pressed(KeyCode::KeyA) {
-        position.translation.x -= 10.0;
+        position.translation.x -= 5.0;
     }
 
     if key.pressed(KeyCode::KeyD) {
-        position.translation.x += 10.0;
+        position.translation.x += 5.0;
     }
 }
+
+fn update_zoom_by_scroll(
+    mut wheel_events: EventReader<MouseWheel>, 
+    mut projection_query: Query<&mut OrthographicProjection, With<CameraEnt>>
+) {
+    for event in wheel_events.read() {
+        let mut projection = projection_query.single_mut();
+        projection.scale = (projection.scale + event.y * 0.1).max(0.1);
+    }
+}
+
+fn update_planets_position(
+    time: Res<Time>,
+    mut query: Query<(&mut Object, &mut Transform), With<Object>>
+) {
+
+    for (mut object, mut transform) in query.iter_mut() {
+        if matches!(object.kind, ObjectKind::Star) {
+            continue;
+        }
+
+        object.angle += object.speed * time.delta().as_secs_f32();
+        let x = object.angle.cos() * object.radius;
+        let y = object.angle.sin() * object.radius;
+        transform.translation = Vec3::new(x, y, 1.);
+    }
+   
+}
+
+fn spawn_orbits(
+    mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, 
+    mut materials: ResMut<Assets<ColorMaterial>>, 
+    query_planets: Query<&Transform, With<Object>>
+) {
+    for planet in query_planets.iter() {
+        let planet_position = planet.translation;
+        if planet_position.x != 0. {
+            let orbit = Annulus::new(planet_position.x - 0.5, planet_position.x + 0.5);
+
+            commands.spawn(
+                (Mesh2d(meshes.add(orbit)), 
+                MeshMaterial2d(materials.add(Color::srgb(0.5, 0.5, 0.5))),
+                Transform::from_xyz(0.,0.,0.1)
+            ));
+        }
+    }
+}
+
